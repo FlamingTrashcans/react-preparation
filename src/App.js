f2d6d285634e8d5313b2dd01cee2f2c2700005d9
Peyton Redman <pwredman@outlook.com>
2026-03-22 16:20:31 -0500

    Updated March 22

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
 
 function App() {
- <Router> 
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route></Route>
-      </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='id' element={<Posts />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
- </Router> 
 }
 
 export default App;
